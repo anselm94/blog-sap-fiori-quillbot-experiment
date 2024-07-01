76572cfa051f8e38e107d030e2c702ca9044e847
Merbin J Anselm <merbin.j.anselm@sap.com>
2024-07-01 20:18:26 +0530

    chore: cleanup `app/AIWriter` directory

--- app/AIWriter/webapp/Component.ts
+++
@@ -1,9 +0,0 @@
-import AppComponent from "sap/fe/core/AppComponent";
-
-export default class Component extends AppComponent {
-  public static readonly metadata = {
-    manifest: "json",
-  };
-
-  public init(): void {}
-}
