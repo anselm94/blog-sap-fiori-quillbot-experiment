6341c72fa5de4e14e243002066260c413ef42529
Merbin J Anselm <merbin.j.anselm@sap.com>
2024-07-01 22:13:26 +0530

    fix: typescript ui loading

--- app/aiwriter/webapp/Component.ts
+++ app/aiwriter/webapp/Component.ts
@@ -1,9 +1,14 @@
 import AppComponent from "sap/fe/core/AppComponent";
 
+/**
+ * @namespace aiwriter
+ */
 export default class Component extends AppComponent {
   public static readonly metadata = {
     manifest: "json",
   };
 
-  public init(): void {}
+  public init(): void {
+    super.init();
+  }
 }
